54c2fb8483efae938eec30d146c3aa189d872017
davidhenley <henleydr@gmail.com>
2017-02-13 17:37:13 -0600

    Fineshed Button to Link to Safari

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { Text, Image, View } from 'react-native';
+import { Text, Image, View, Linking } from 'react-native';
 
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({album}) => {
-  const { title, artist, thumbnail_image, image } = album;
+  const { title, artist, thumbnail_image, image, url } = album;
   const { thumbnailStyle, headerText, thumbnailContainer, textContainer, albumStyle } = styles;
   return (
     <Card>
@@ -21,6 +22,12 @@ const AlbumDetail = ({album}) => {
 
       <CardSection>
         <Image style={albumStyle} source={{ uri: image }} />
+      </CardSection>
+
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url)}>
+          Buy Now
+        </Button>
       </CardSection>
     </Card>
   );
