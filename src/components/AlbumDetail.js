f55d0c80b73795d6d2e0ae3ef41153955ae397ab
davidhenley <henleydr@gmail.com>
2017-02-13 17:01:34 -0600

    Album Cards Done

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,17 +1,53 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, Image, View } from 'react-native';
 
 import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({album}) => {
+  const { title, artist, thumbnail_image, image } = album;
+  const { thumbnailStyle, headerText, thumbnailContainer, textContainer, albumStyle } = styles;
   return (
     <Card>
       <CardSection>
-        <Text>{album.title}</Text>
+        <View style={thumbnailContainer}>
+          <Image style={thumbnailStyle} source={{ uri: thumbnail_image }} />
+        </View>
+        <View style={textContainer}>
+          <Text style={headerText}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+
+      <CardSection>
+        <Image style={albumStyle} source={{ uri: image }} />
       </CardSection>
     </Card>
   );
 };
+
+const styles = {
+  textContainer: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerText: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50,
+  },
+  thumbnailContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  albumStyle: {
+    height: 300,
+    flex: 1
+  }
+}
 
 export default AlbumDetail;
