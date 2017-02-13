2d17b1c6a500cfbff50079f708c05725eeaaafe4
davidhenley <henleydr@gmail.com>
2017-02-13 15:39:49 -0600

    Album List and Detail

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { View } from 'react-native';
 
-import Header from './components/header';
+import Header from './components/Header';
+import AlbumList from './components/AlbumList';
 
 const App = () => {
   return (
-    <Header title={'Albums'} />
+    <View>
+      <Header title={'Albums'} />
+      <AlbumList />
+    </View>
   );
 }
 
